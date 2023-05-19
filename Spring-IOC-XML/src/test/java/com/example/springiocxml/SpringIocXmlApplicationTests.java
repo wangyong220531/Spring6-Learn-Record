b70cf4efee13459bbc0d04cd049fc3f493340fda
王勇 <wy19945372694@163.com>
2023-05-19 10:35:22 +0800

    first commit

---
+++ Spring-IOC-XML/src/test/java/com/example/springiocxml/SpringIocXmlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springiocxml;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringIocXmlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
